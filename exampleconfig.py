bf47fdcbc2be2e5d28b455d3f3a265cd1a0b3400
Mᴜᴊʜᴇ ᴋʏᴀ ᴍᴇ ᴛᴏ ʙᴀᴄᴄʜᴀ ʜᴜ <125765947+CuteBaccha@users.noreply.github.com>
2023-02-24 00:06:07 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -16,14 +16,14 @@ BOT_TOKEN10 =
 
 SUDO_USERS = 
 
-bot = TelegramClient('LegendBoy', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
-bot2 = TelegramClient('LegendBoy2', APP_ID, API_HASH).start(bot_token=BOT_TOKEN2) 
-bot3 = TelegramClient('LegendBoy3', APP_ID, API_HASH).start(bot_token=BOT_TOKEN3) 
-bot4 = TelegramClient('LegendBoy4', APP_ID, API_HASH).start(bot_token=BOT_TOKEN4) 
-bot5 = TelegramClient('LegendBoy5', APP_ID, API_HASH).start(bot_token=BOT_TOKEN5) 
-bot6 = TelegramClient('LegendBoy6', APP_ID, API_HASH).start(bot_token=BOT_TOKEN6) 
-bot7 = TelegramClient('LegendBoy7', APP_ID, API_HASH).start(bot_token=BOT_TOKEN7) 
-bot8 = TelegramClient('LegendBoy8', APP_ID, API_HASH).start(bot_token=BOT_TOKEN8) 
-bot9 = TelegramClient('LegendBoy9', APP_ID, API_HASH).start(bot_token=BOT_TOKEN9) 
-bot10 = TelegramClient('LegendBoy10', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10)
+bot = TelegramClient('Hellspam ', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+bot2 = TelegramClient('Hellspam 2', APP_ID, API_HASH).start(bot_token=BOT_TOKEN2) 
+bot3 = TelegramClient('Hellspam 3', APP_ID, API_HASH).start(bot_token=BOT_TOKEN3) 
+bot4 = TelegramClient('Hellspam 4', APP_ID, API_HASH).start(bot_token=BOT_TOKEN4) 
+bot5 = TelegramClient('Hellspam 5', APP_ID, API_HASH).start(bot_token=BOT_TOKEN5) 
+bot6 = TelegramClient('Hellspam 6', APP_ID, API_HASH).start(bot_token=BOT_TOKEN6) 
+bot7 = TelegramClient('Hellspam 7', APP_ID, API_HASH).start(bot_token=BOT_TOKEN7) 
+bot8 = TelegramClient('Hellspam 8', APP_ID, API_HASH).start(bot_token=BOT_TOKEN8) 
+bot9 = TelegramClient('Hellspam 9', APP_ID, API_HASH).start(bot_token=BOT_TOKEN9) 
+bot10 = TelegramClient('Hellspam 10', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10)
 
